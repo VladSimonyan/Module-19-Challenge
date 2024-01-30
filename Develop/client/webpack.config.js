a8195bdcca4594f96ada7141daa4b408ba65f79a
simon <simon@DESKTOP-HUECNQL>
2024-01-29 20:41:39 -0800

    changes and updates

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -7,24 +7,71 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+    return {
+        mode: 'development',
+        entry: {
+            main: './src/js/index.js',
+            install: './src/js/install.js'
+        },
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        plugins: [
+            // Generates our html file.
+            new HtmlWebpackPlugin({
+                template: './index.html',
+                title: 'J.A.T.E'
+            }),
 
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+            // Injects our custom service worker
+            new InjectManifest({
+                swSrc: './src-sw.js',
+                swDest: 'src-sw.js',
+            }),
+
+            // Creates a manifest.json file.
+            new WebpackPwaManifest({
+                fingerprints: false,
+                inject: true,
+                name: 'Just Another Text Editor',
+                short_name: 'JATE',
+                description: 'Text Editor',
+                background_color: '#225ca3',
+                theme_color: '#225ca3',
+                start_url: '/',
+                publicPath: '/',
+                icons: [
+                    {
+                        src: path.resolve('src/images/logo.png'),
+                        sizes: [96, 128, 192, 256, 384, 512],
+                        destination: path.join('assets', 'icons'),
+                    },
+                ],
+            }),
+
+        ],
+
+        module: {
+            // CSS Loaders
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    // babel-loader
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+                        },
+                    },
+                },
+            ],
+        },
+    };
 };
